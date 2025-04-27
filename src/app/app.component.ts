376c4b6edea29ef48c2896925fb8b946d0e8da87
laziz9ek <lazizbek6020414@gmail.com>
2025-04-27 06:21:10 +0300

    fix: bug with text register changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,19 +2,15 @@ import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+const menuItems: string[] = ['Каталог', 'Стройматериалы', 'Инструменты', 'Электрика', 'Интерьер и одежда'];
 
-const menuItems: string[] = ['Каталог', 'Стройматериалы', 'Инструменты', 'Электрика', 'Интерьер и одежда']
-const upperCaseMenuItems:string[] = menuItems.map(
-  (item:string) => {
+const upperCaseMenuItems: string[] = menuItems.map(
+  (item) => {
     return item.toUpperCase();
   }
 );
-const newPages: number[] = [5, 4, 3, 2, 1]
 
-//const headerItem2 = 'О компании';
-
-const company = (name:string) => name;
-const aboutCompany:string = company('О компании')
+const newPages: number[] = [5, 4, 3, 2, 1];
 
 @Component({
   selector: 'app-root',
@@ -25,39 +21,38 @@ const aboutCompany:string = company('О компании')
 })
 export class AppComponent {
   title = 'mentoring-first-project';
-  
-  isShowCatalog:boolean = true;
-  text:string = 'Click';
 
-  isShowBanner:boolean = true;
+  isShowCatalog: boolean = true;
+  isShowBanner: boolean = true;
+
+  text: string = 'Click';
 
   readonly topHeaderBarItem1 = 'Главная';
-  readonly aboutCompany = company(aboutCompany);
+  readonly aboutCompany = 'О компании';
   readonly catalog = 'Каталог';
   readonly orderBtn = 'Пусто';
   readonly phoneNumber = '+7 (965) 084-29-29';
 
-  readonly bottomHeaderBarItem1 = 'Каталог';
-  readonly bottomHeaderBarItem2 = 'Стройматериалы';
+  readonly header2Item1 = upperCaseMenuItems[0];
+  readonly bottomHeaderBarItem2 = menuItems[1];
   readonly bottomHeaderBarItem3 = 'Инструменты';
   readonly bottomHeaderBarItem4 = 'Электрика';
   readonly bottomHeaderBarItem5 = 'Интерьер и одежда';
   readonly location = 'Москва';
 
-  readonly header2Items:string = upperCaseMenuItems[0];
-  
-  readonly newPages:number[] = newPages;
+  menuItems: string[] = upperCaseMenuItems;
+  newPages: number[] = newPages;
 
+  isUpperCase = true;
 
-  menuItems:string[] = upperCaseMenuItems;
-
-  isUpperCase:boolean = true;
-
-  changeMenuText():void {
+  changeMenuText() {
     this.menuItems = upperCaseMenuItems.map(
       item => this.isUpperCase ? item.toLowerCase() : item.toUpperCase()
     )
-    this.isUpperCase = !this.isUpperCase
+    this.isUpperCase = !this.isUpperCase;
   }
+
+  
+
 
 }
